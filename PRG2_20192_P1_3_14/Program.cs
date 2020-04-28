09d31424e71cd003ad32de50879a03b3d7aa2d57
Harvianti Hasna Putri <Harviantiputrii@gmail.com>
2020-04-28 15:12:53 +0700

    initial commit

---
+++ PRG2_20192_P1_3_14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PRG2_20192_P1_3_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 10;
+            Console.WriteLine("PROGRAM LAGU ANAK AYAM");
+            Console.WriteLine("======================");
+          
+           
+            int i;
+
+            for (i = N; i > 1; i--)
+            {
+                Console.WriteLine (" anak ayam turun {0}", i);
+                Console.Write("mati satu tinggal induknya {0}", i - 1);
+               
+            }
+            Console.Write("Anak ayam turun 1, mati satu tinggal induknya");
+        }
+    }
+}
+
+
+
